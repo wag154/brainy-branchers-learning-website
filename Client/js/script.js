b185a2f5299b80a87b24385d6f02568127c04383
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-06 11:49:24 +0000

    added function to check if letter is in word

--- Client/js/script.js
+++ Client/js/script.js
@@ -1,5 +1,28 @@
 const HangmanDisplay = document.querySelector("#Display-Hangman div");
 const EnteredLetter = document.querySelector("#Interaction form");
+const ExampleWords = ["hello","world","put","bin","wheel","fool","drool","pool"]
+
+let max = ExampleWords.length;
+let min = 1;
+let correct = 0;
+let incorrect = 0;
+let random = Math.floor(Math.random()*(max+min) - min)
+let word =ExampleWords[random-1];
+
+const DisplayHang = () =>{
+  
+}
+
+const getLetter = (letter) =>{
+  if (word.includes(letter)){
+    correct ++;
+  }
+  else {
+    incorrect ++
+  }
+  DisplayHang()
+}
+
 
 const CheckLetter = (e) =>{
   e.preventDefault();
@@ -13,12 +36,10 @@ const CheckLetter = (e) =>{
 
     alert("Please Enter A Letter, Not A Number!")
   }
-  
+  else {
+    getLetter(Letter)
+  }
   e.target.UserInput.value = "";
-}
-
-const getLetter = (letter) =>{
-
 }
 
 EnteredLetter.addEventListener("submit",CheckLetter);
