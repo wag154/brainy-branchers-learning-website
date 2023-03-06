c9293bac131e84af1de218086956581d636656c4
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-06 11:07:10 +0000

    added function to check if value is a string

--- Client/js/script.js
+++ Client/js/script.js
@@ -1,1 +1,17 @@
 const HangmanDisplay = document.querySelector("#Display-Hangman div");
+const EnteredLetter = document.querySelector("#Interaction");
+
+const CheckLetter = () =>{
+  e.preventDefault();
+  let letter = e.target.UserInput.value;
+
+  if (typeof letter != "string"){
+    alert("Please Enter A Letter, Not A Number!")
+  }
+}
+
+const getLetter = (letter) =>{
+
+}
+
+EnteredLetter.addEventListener("submit",CheckLetter);
