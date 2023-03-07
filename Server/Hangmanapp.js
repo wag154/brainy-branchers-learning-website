baad04011af94f1006a815f1a821d6b6436e3c01
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-07 16:46:53 +0000

    fixed getting wrong app

--- Server/Hangmanapp.js
+++ Server/Hangmanapp.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require ("cors");
 const hangWords = require("./Hangman.json");
 const app = express();
-const EnglishLang = require("./HangmanInformation.json")
-const getExamQuestions = require("./exam.json")
+const EnglishLang = require("./HangmanInformation.json");
+const getExamQuestions = require("./exam.json");
 
 app.use(express.json());
 app.use(cors());
@@ -11,10 +11,17 @@ app.use(cors());
 
 app.use ("/HangWords", (req,res)=>{
   res.send(hangWords)
-  })
+  });
+
+app.use("/Questions/:subject", (req,res) =>{
+ // res.send("Hello worlds")
+  res.send(getExamQuestions[req.params.subject])
+});
 
 app.use ("/lecture",(req,res)=>{
+
   res.send(EnglishLang)
+
 })
 
 
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 
 require("dotenv").config();
-const app = require("./login.js");
+const app = require("./Hangmanapp");
 const port = process.env.PORT;
 
 app.listen(port, () => {
