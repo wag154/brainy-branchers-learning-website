cdb4147b095551480adfff36413252ff0b9ae63b
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-07 16:11:21 +0000

    added js selectors

---
+++ Client/js/ExamGet.js
@@ -0,0 +1,5 @@
+const QuestionDis = document.querySelector("#questions h1");
+const option1Dis = document.querySelector("#option1");
+const option2Dis = document.querySelector("#option2");
+const option3Dis = document.querySelector("#option3");
+
