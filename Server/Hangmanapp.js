a5aa3b7a3d39c3c4b51fb9280bbbb27ab03153af
xonic5461@gmail.com <85699535+wag154@users.noreply.github.com>
2023-03-07 10:55:39 +0000

    added hangman information

--- Server/Hangmanapp.js
+++ Server/Hangmanapp.js
@@ -2,18 +2,18 @@ const express = require("express");
 const cors = require ("cors");
 const hangWords = require("./Hangman.json");
 const app = express();
+const EnglishLang = require("./HangmanInformation.json")
+
 app.use(express.json());
 app.use(cors());
 
 
 app.use ("/HangWords", (req,res)=>{
+  res.send(hangWords)
+  })
+
 app.use ("/lecture",(req,res)=>{
-
-  
-
-}
-)
-res.send(hangWords)
+  res.send(EnglishLang)
 })
 
 module.exports = app;
