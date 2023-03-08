8d1df7be450fec65e3a8977e872101949b47124c
Jack <jackkevinperkins03@gmail.com>
2023-03-08 11:23:09 +0000

    fixed some css, added chaning header

--- Client/js/SignUp.js
+++ Client/js/SignUp.js
@@ -1,12 +1,35 @@
 const LoginDis = document.querySelector("#LoginDis form");
+const DisNameLabel = document.querySelector("#NameLabel");
+const dIsNameInput = document.querySelector("#NameInput");
+const BtnSwitch = document.querySelector("#LoginBtn")
+const HeadDis = document.querySelector("#Displayer");
+const signUpOption = true;
 
+const check =(data) =>{
+  if(data.includes(true)) {
+
+  }
+}
+async function Login (userName,password){
+  try {
+    const resp  = await fetch (`http://127.0.0.1:3000/Login/${userName}&${password}`)
+    if (resp.ok){
+     let data = await resp.json();
+     check(data)
+    }
+    else {
+      throw "HTTP ERROR CODE:" + resp.status;
+    }
+  }
+  catch{((e) => console.log(e))}
+}
 
 async function SignUp(username,password,name){
 
   try {
     const resp = await fetch (`http://127.0.0.1:3000/UserInfo/${username}&${password}&${name}`)
     if (resp.ok){
-      data = await resp.json()
+      let data = await resp.json()
     }
     else {
       throw "HTTP ERROR CODE :" + resp.status;
@@ -44,13 +67,34 @@ const getUserInfo = (e) =>{
     alert("Please Enter A Name")
   }
   else {
-    SignUp(userName,passWord,UserNickName)
+    if (signUpOption == true){
+      SignUp(userName,passWord,UserNickName)
+    }
+    else {
+      Login(userName,passWord)
+    }
   }
 
   e.target.userName.value = "";
   e.target.Password.value = "";
 } 
 
+function LoginBtn (){
+  if (BtnSwitch.textContent == "Sign Up"){
+    DisNameLabel.style.display = "block"
+    dIsNameInput.style.display = "block"
+    BtnSwitch.textContent = "Login";
+    HeadDis.textContent = "Sign Up";
+    signUpOption = false;
+  }
+  else {
+    DisNameLabel.style.display = "none";
+    dIsNameInput.style.display = "none";
+    BtnSwitch.textContent = "Sign Up";
+    HeadDis.textContent = "Login";
+    signUpOption = true;
+  }
+}
 
 
 
